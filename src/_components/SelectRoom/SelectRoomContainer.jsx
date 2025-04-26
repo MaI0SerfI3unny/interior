11908b488b99b32315afff0d9d92892b1204e402
Hanna Lakus <annalakus@ukr.net>
2025-04-27 00:04:15 +0100

    fixed name button

--- src/_components/SelectRoom/SelectRoomContainer.jsx
+++ src/_components/SelectRoom/SelectRoomContainer.jsx
@@ -1,27 +1,26 @@
-
 import {
   SelectRoomContainerStyles,
   SelectWrapper,
   ArrowContainer,
-} from './SelectRoomContainerStyles.styled'
-import {ReactComponent as ArrowIcon} from '../../svg/vector.svg'
-import OptionElement from './OptionElement/OptionElement'
-import { Field } from 'formik'
+} from "./SelectRoomContainerStyles.styled";
+import { ReactComponent as ArrowIcon } from "../../svg/vector.svg";
+import OptionElement from "./OptionElement/OptionElement";
+import { Field } from "formik";
 
-const initialValues = [
-  { text: 'Кухня', value: 'citchen' },
-  { text: 'Студія', value: 'studio' },
-  { text: 'Вітальня', value: 'hello' },
-  { text: 'Ванна кімната', value: 'bathroom' },
-  { text: 'Спальня', value: 'sleep' },
-]
+// const initialValues = [
+//   { text: "Кухня", value: "kitchen" },
+//   { text: "Студія", value: "studio" },
+//   { text: "Вітальня", value: "hello" },
+//   { text: "Ванна кімната", value: "bathroom" },
+//   { text: "Спальня", value: "sleep" },
+// ];
 
-const SelectRoomContainer = ({initialValues}) => {
+const SelectRoomContainer = ({ initialValues }) => {
   return (
     <SelectRoomContainerStyles>
       <h2>Обрати кімнату</h2>
       <SelectWrapper>
-        <Field as='select' name="room"  >
+        <Field as="select" name="room">
           {initialValues.map(({ text, value }) => (
             <OptionElement key={value} value={value} text={text} />
           ))}
@@ -31,7 +30,7 @@ const SelectRoomContainer = ({initialValues}) => {
         </ArrowContainer>
       </SelectWrapper>
     </SelectRoomContainerStyles>
-  )
-}
+  );
+};
 
-export default SelectRoomContainer
+export default SelectRoomContainer;
