d429455e1fb209e2123992322aa6e9870bc23f35
daniledkin <daniledkin@gmail.com>
2025-04-26 18:38:29 +0300

    fix faq link && update header link

--- src/_components/App/App.jsx
+++ src/_components/App/App.jsx
@@ -2,10 +2,9 @@ import { Route, Routes } from "react-router-dom";
 import { LandingPage } from "@/landing/LandingPage.jsx";
 import { SignInPage } from "../../pages/SignInPage.jsx";
 import { SignUpPage } from "../../pages/SignUpPage.jsx";
-import { FAQPage } from "../../pages/FAQPage/FAQPage.jsx";
+import { FAQPage } from "../../pages/FAQPage.jsx";
 import GeneratePage from "../../pages/GeneratePage.jsx";
 import Layout from "../Layout/Layout.jsx";
-import { SharedLayout } from "../SharedLayout/SharedLayout.jsx";
 import { SendRecoveryEmail } from "../../pages/SendRecoveryEmail.jsx";
 import { RecoveryPwd } from "../../pages/RecoveryPwd.jsx";
 
