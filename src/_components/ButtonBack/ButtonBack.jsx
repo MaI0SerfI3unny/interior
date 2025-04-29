ce3cc3ae1783b778a2ccc2f45e9f95b25ed27740
Hanna Lakus <annalakus@ukr.net>
2025-04-29 23:01:46 +0100

    added translite for signin page

--- src/_components/ButtonBack/ButtonBack.jsx
+++ src/_components/ButtonBack/ButtonBack.jsx
@@ -1,12 +1,15 @@
 import { Link } from "react-router-dom";
 import { ReactComponent as Arrow } from "../../assets/icons/arrow.svg";
 import css from "./ButtonBack.module.scss";
+import { useTranslation } from "react-i18next";
 
 export const ButtonBack = ({ path }) => {
+  const { t } = useTranslation();
+
   return (
     <Link className={css.arrowBack} to={path}>
       <Arrow />
-      <span>Back</span>
+      <span>{t("login.buttonBack")}</span>
     </Link>
   );
 };
--- src/i18n.js
+++ src/i18n.js
@@ -1,41 +1,46 @@
 // src/i18n.js
-import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
-import LanguageDetector from 'i18next-browser-languagedetector';
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+import HttpBackend from "i18next-http-backend";
 
 i18n
+  .use(HttpBackend)
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    fallbackLng: 'ua',
+    fallbackLng: "ua",
     debug: false,
     interpolation: {
       escapeValue: false,
     },
-    resources: {
-      ua: {
-        translation: {
-            nav:{
-                home: "Головна",
-                generate: "Генерація",
-                subscribe: "Підписка",
-                faq: "FAQ",
-                login: "Вхід/Реєстрація"
-            }
-        }
-      },
-      en: {
-        translation: {
-            nav: {
-                home: "Home",
-                generate: "Generation",
-                subscribe: "Subscribe",
-                faq: "FAQ",
-                login: "Sign in / Register"
-            }
-        }
-      }
-    }
+    backend: {
+      loadPath: "/locales/{{lng}}/translation.json",
+    },
+    // resources: {
+    //   ua: {
+    //     translation: {
+    //       nav: {
+    //         home: "Головна",
+    //         generate: "Генерація",
+    //         subscribe: "Підписка",
+    //         faq: "FAQ",
+    //         login: "Вхід/Реєстрація",
+    //       },
+    //     },
+    //   },
+    //   en: {
+    //     translation: {
+    //       nav: {
+    //         home: "Home",
+    //         generate: "Generation",
+    //         subscribe: "Subscribe",
+    //         faq: "FAQ",
+    //         login: "Sign in / Register",
+    //       },
+    //     },
+    //   },
+    // },
   });
 
 export default i18n;
