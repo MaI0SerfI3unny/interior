4f6a4d8042ab50851537da6fdfe9ce64fb5dc942
argojkee <maksymhaidabura@gmail.com>
2025-06-05 14:07:23 +0300

    changed banner title language when changed language page without reload page

--- src/landing/_components/Banner/BannerHeading/BannerHeading.jsx
+++ src/landing/_components/Banner/BannerHeading/BannerHeading.jsx
@@ -6,6 +6,11 @@ export const BannerHeading = ({ text }) => {
   const [index, setIndex] = useState(0);
 
   useEffect(() => {
+    setDisplayedText("");
+    setIndex(0);
+  }, [text]);
+
+  useEffect(() => {
     if (index < text.length) {
       const timeout = setTimeout(() => {
         setDisplayedText(prev => prev + text[index]);
